98b57019a326759d68c9d1b2259ec23936bf7354
Guilherme Leon Berno de Jesus <guileon1234@gmail.com>
2012-11-22 21:17:44 -0200

    quas epronto falta testar
    FALTA TESTAR PORRA

--- sisop/main.c
+++ sisop/main.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#define BOOL int
+#define TRUE 1;
+#define FALSE 0;
+
+
+int mv_start(int npages, int nframes);
+int mv(int page, int *frame);
+int mv_stop(void);
+
+
+int number_pages_in_memory=0;
+int max_memory;
+int max_pages;
+pagelist * memory = NULL;
+pagelist * disk = NULL;
 
 
 typedef struct pagelist
@@ -8,7 +23,7 @@ typedef struct pagelist
 	struct pagelist * next;
 } pagelist;
 
-pagelist * insert(pagelist* list, int _page, int _frame)
+pagelist * insert(pagelist* list, int _page, int _frame) // insere um elemento na última posição da lista
 {
 
 	pagelist * new = (pagelist*)malloc(sizeof(pagelist));
@@ -35,7 +50,7 @@ pagelist * insert(pagelist* list, int _page, int _frame)
 
 }
 
-void print(pagelist* list)
+void print(pagelist* list) // imprime a lista
 {
 	pagelist * p1 = list;
 	while(p1!=NULL)
@@ -45,32 +60,150 @@ void print(pagelist* list)
 	}
 }
 
-pagelist * _remove(pagelist * list, int * page_number, int * frame_number)
+pagelist * _remove(pagelist * list, int * page_number, int * frame_number) // remove o primeiro elemento da lista
 {
+    pagelist * p1;
 	*page_number = list->page;
 	*frame_number = list->frame;
-	return list->next;
+	p1 = list->next;
+	free(list);
+	return p1;
 }
 
-int is_in_list(pagelist * list, int page_number)
+pagelist * free_(pagelist * list) // desaloca o ultimo elemento da lista
+{
+    pagelist * p1 = NULL;
+    pagelist * p2 = list;
+    while(p2!=NULL)
+    {
+        p1 = p2;
+        p2 = p1->next;
+    }
+    free(p1);
+}
+
+void free_all(pagelist * list) // desaloca todos os elementos da lista
+{
+    while(list!=NULL)
+    {
+        list = free_(list);
+    }
+}
+
+int get_frame(int page) // retorna o a frame que contém a página de número page
+{
+    pagelist * p1 = memory;
+    while(p1)
+    {
+        if (p1->page == page)
+            return p1->frame;
+    }
+}
+
+BOOL is_in_list(pagelist * list, int page_number) // testa se a página de número page_number está na lista
 {
 
-	int retvalue = 0;
+	BOOL retvalue = FALSE;
 	pagelist * p1 = list;
 	while(p1!=NULL)
 	{
 		if (p1->page == page_number)
-			retvalue = 1;
+			retvalue = TRUE;
 		p1 = p1->next;
 	}
 }
 
 
+BOOL is_in_memory(int _page) // testa se a página de número _page está na lista
+{
+    return is_in_list(memory,_page);
+}
+
+BOOL is_memory_full() // testa se a memória está cheia
+{
+    if(number_pages_in_memory < max_memory)
+        return FALSE;
+    else
+        return TRUE;
+}
+
+void insert_in_memory_in_frame(int page, int frame) // insere a página de número page no frame;
+{
+    pagelist * p1 = memory;
+    while(p1!=NULL)
+    {
+        if(p1->frame == frame)
+            p1->page = page;
+    }
+}
+
+void insert_in_disk(int page) // insere a página de número page no disco
+{
+    disk = insert(disk,page,-1);
+}
+
+void remove_from_disk(int page) // remove a página de número page do disco
+{
+    pagelist * p1 = NULL;
+    pagelist * p2 = disk;
+    while(p2->page != page)
+    {
+        p1 = p2;
+        p2 = p2->next;
+    }
+    p1->next = p2->next;
+}
+
+int insert_in_memory(int _page) // se a memória não está cheia, coloca a pagina na proxima frame livre, caso a memória esteja cheia, faz o swap
+{
+    int frame;
+    if (!is_memory_full())
+    {
+        remove_from_disk(_page);
+        memory = insert(memory, _page, number_pages_in_memory);
+        frame = number_pages_in_memory;
+        number_pages_in_memory++;
+
+    }
+    else
+    {
+        int pn,fn;
+        memory = _remove(memory, &pn, &fn);
+        memory = insert(memory,_page,fn);
+        remove_from_disk(_page);
+        insert_in_disk(pn);
+        frame = fn;
+    }
+    return frame;
+
+}
+
+int mv_start(int npages, int nframes)
+{
+    int i=0;
+    for (i=0 ; i<npages ; i++)
+        disk = insert_in_disk(i);
+    max_memory = nframes;
+}
+
+int mv(int page, int *frame)
+{
+        if(is_in_memory(page))
+            *frame = get_frame(page);
+        else
+            *frame = insert_in_memory(page);
+        return 0;
+}
+
+int mv_stop(void)
+{
+    free_all(memory);
+    free_all(disk);
+}
 
 int main()
 {
-	pagelist * memory = NULL;
-	pagelist * disk = NULL;
+
 
 
 }
